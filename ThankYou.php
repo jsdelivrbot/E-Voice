0f685c05dbe12a9312f93937fd231614b48a31f8
WindowsGit <austinstevens99@hotmail.com>
2018-05-21 15:42:13 -0400

    First Commit

---
+++ ThankYou.php
@@ -0,0 +1,48 @@
+<!doctype Html>
+<head>
+  <title>Sent!</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+    integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet"  href="style.css">
+</head>
+<body>
+
+    <header>
+      <div id="BestBuyLogo">
+        <img src="Logo.png" alt="BestBuy Logo">
+      </div>
+
+      <div id="HomeButton">
+        <a href="Home.html">Home</a>
+      </div>
+
+      <div id="DisscusionButton">
+        <a href="Discussion.html">Discuss</a>
+      </div>
+
+      <div id="FeedbackButton">
+        <a href="Feedback.html">Feedback</a>
+      </div>
+    </header>
+
+<div id="ThankYou">
+  <h3>Thank you!</h3>
+    <p>Your info has been submitted. Thanks for your time and effort to make
+      our store the best it can be!</p>
+</div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="./js/jquery.validate.min.js"></script>
+
+<script src="./js/tether.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+<div id="gif">
+  <img src="https://media0.giphy.com/media/O5NQWAFrDwIZW/giphy.gif"/>
+</div>
+
+</body>
