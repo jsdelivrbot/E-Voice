c415d7ebd9680ba061b1ddb20970a51c5ca92e12
WindowsGit <austinstevens99@hotmail.com>
2018-06-18 18:26:38 -0400

    Added function to read index.html through index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + '/public'))
@@ -11,3 +12,15 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  fs.readFile('./index.html', null, function(error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found');
+  } else {
+      response.write(data);
+  }
+    response.end();
+});
