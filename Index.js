153ad897ef12bdd27c84da8bb46cff3e14ff64e0
WindowsGit <austinstevens99@hotmail.com>
2018-06-18 18:34:57 -0400

    Added "E-Voice" path before index.html on line 19

--- Index.js
+++ Index.js
@@ -13,9 +13,10 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
+// This function was added to try and read the HTML file from the E-Voice folder.
 function onRequest(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  fs.readFile('./index.html', null, function(error, data) {
+  fs.readFile('./E-Voice/index.html', null, function(error, data) {
     if (error) {
       response.writeHead(404);
       response.write('File not found');
