aa7afb788a4e8734794fcfac06d47ef01f8c57ba
WindowsGit <austinstevens99@hotmail.com>
2018-07-07 09:21:54 -0400

    committing vendor files for php mailer in server to work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 80))
 app.use(express.static(__dirname + '/public'))
@@ -8,8 +9,24 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
+
+/** Function to read html file **/
+
+// function onRequest(request, response) {
+// response.writeHead(200, {'Content-Type': 'text/plain'});
+// fs.readFile('./index.html', null, function(error, data) {
+//   if (error) {
+//       response.writeHead(404);
+//       response.write('File not found!');
+//     } else {
+//         response.write(data);
+//     }
+//     response.end()
+// });
+// }
+//
+// http.createServer(onRequest).listen(8000);
