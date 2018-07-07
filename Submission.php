87ec39e93215de0aa335eeed4610dc28bc67a7fd
WindowsGit <austinstevens99@hotmail.com>
2018-07-07 17:18:55 -0400

    updated smtp2go email and password

--- Submission.php
+++ Submission.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; 
 
 
 
@@ -33,8 +33,8 @@ try {
     $mail->isSMTP();
     $mail->Host = 'mail.smtp2go.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'bestofblueproject@hotmail.com';
-    $mail->Password = 'Tayler19';
+    $mail->Username = 'dsblade0@hotmail.com';
+    $mail->Password = '1403Stevens';
     $mail->SMTPSecure = 'tls';
     $mail->Port = "2525";
 
